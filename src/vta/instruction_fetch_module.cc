974e62b1512f3469fec1a5139aeb71b50fb3a0ed
zrq8 <lhz@live.cn>
2023-12-13 22:26:37 +0800

    vta: initial implementation of InstructionFetchModule
    Change-Id: I441bedad58a21842847ae72253b17d8f5e287516

--- src/vta/instruction_fetch_module.cc
+++ src/vta/instruction_fetch_module.cc
@@ -2,6 +2,8 @@
 
 #include "base/trace.hh"
 #include "debug/BaseVTAFlag.hh"
+#include "vta/vta.hh"
+#include "vta/vta_const.hh"
 
 namespace gem5
 {
@@ -12,5 +14,34 @@ InstructionFetchModule::InstructionFetchModule(const Params &params) :
     storeCommandQueue(params.store_command_queue)
 {
     DPRINTF(BaseVTAFlag, "create InstructionFetchModule!\n");
+
+    vta::Instruction *instruction_vec{};
+    size_t instruction_count{};
+    for (size_t pc{}; pc != instruction_count; ++pc) {
+        const auto instruction{instruction_vec[pc]};
+        const auto opcode{instruction.opcode()};
+        switch (opcode) {
+        case vta::Opcode::LOAD:
+            if (const auto memory_type{
+                    instruction.asMemoryInstruction().memory_type};
+                memory_type == vta::MemoryId::INPUT ||
+                memory_type == vta::MemoryId::WEIGHT) {
+                DPRINTF(BaseVTAFlag, "instruction to load\n");
+                // loadCommandQueue << instruction;
+            } else {
+                DPRINTF(BaseVTAFlag, "instruction to compute\n");
+                // computeCommandQueue << instruction;
+            }
+            break;
+        case vta::Opcode::STORE:
+            DPRINTF(BaseVTAFlag, "instruction to store\n");
+            // storeCommandQueue << instruction;
+            break;
+        default:
+            DPRINTF(BaseVTAFlag, "instruction to compute\n");
+            // computeCommandQueue << instruction;
+            break;
+        }
+    }
 }
 } // namespace gem5

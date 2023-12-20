ce7f1eabef677c7bcecb8bf2f3f1cd9f6da9cba0
Yibo Zhang <yb_zhang@mail.ustc.edu.cn>
2023-12-21 00:28:03 +0800

    vta: Adding event to the store module
    Change-Id: I43af89cf4769f67e13c834a8b8df4acdd20c2a76

--- src/vta/store_module.cc
+++ src/vta/store_module.cc
@@ -5,13 +5,79 @@
 
 namespace gem5
 {
+StoreModule::StoreModuleWorkingEvent::StoreModuleWorkingEvent(
+    gem5::StoreModule *storeModule) :
+    storeModule(storeModule)
+{}
+
+auto
+StoreModule::StoreModuleWorkingEvent::process() -> void
+{
+    while (
+        storeModule->instruction || !storeModule->storeCommandQueue->empty()) {
+        switch (storeModule->status) {
+        case StoreModuleStatus::WaitToRead:
+            if (storeModule->instruction == nullptr) {
+                // get instruction from queue
+            }
+            if (/*insn.pop_prev_dep*/ true) {
+                if (storeModule->computeToStoreQueue->tryPop()) {
+                    storeModule->status = StoreModuleStatus::Normal;
+                } else {
+                    return;
+                }
+            }
+
+            break;
+        case StoreModuleStatus::Normal:
+            // do memcpy
+            break;
+        case StoreModuleStatus::WaitToWrite:
+            if (/*insn.push_prev_dep*/ true)
+                if (storeModule->storeToComputeQueue->tryPush()) {
+                    storeModule->status = StoreModuleStatus::WaitToRead;
+                    // free instruction
+                } else {
+                    storeModule->status = StoreModuleStatus::WaitToWrite;
+                }
+            break;
+        }
+    }
+}
+
 StoreModule::StoreModule(const Params &params) :
     SimObject(params),
     storeCommandQueue(params.store_command_queue),
     computeToStoreQueue(params.compute_to_store_queue),
     storeToComputeQueue(params.store_to_compute_queue),
-    outputBuffer(params.output_buffer)
+    outputBuffer(params.output_buffer),
+    status(StoreModuleStatus::Normal),
+    workingEvent(this)
+
 {
     DPRINTF(BaseVTAFlag, "create StoreModule!\n");
 }
+
+auto
+StoreModule::notifyRead() -> void
+{
+    if (status == StoreModuleStatus::WaitToRead) {
+        status = StoreModuleStatus::Normal;
+        // schedule
+        reschedule(workingEvent, curTick());
+    } else {
+    }
+}
+
+auto
+StoreModule::notifyWrite() -> void
+{
+    if (status == StoreModuleStatus::WaitToWrite) {
+        status = StoreModuleStatus::Normal;
+        // schedule
+        reschedule(workingEvent, curTick());
+    } else {
+    }
+}
+
 } // namespace gem5
--- src/vta/store_module.hh
+++ src/vta/store_module.hh
@@ -2,15 +2,20 @@
 #define STORE_MODULE_HH
 
 #include "params/StoreModule.hh"
+#include "sim/eventq.hh"
 #include "sim/sim_object.hh"
 #include "vta/buffer.hh"
+#include "vta/data_queue.hh"
 #include "vta/instruction_queue.hh"
+#include "vta/status.hh"
+#include "vta/vta.hh"
 
 namespace gem5
 {
 class StoreModule : public SimObject
 {
   private:
+    using StoreModuleStatus = gem5::StoreModuleStatus;
     InstructionQueue *storeCommandQueue;
 
     DataQueue *computeToStoreQueue;
@@ -18,10 +23,30 @@ class StoreModule : public SimObject
 
     Buffer *outputBuffer;
 
+    StoreModuleStatus status;
+
+    class StoreModuleWorkingEvent : public Event
+    {
+      private:
+        StoreModule *storeModule;
+
+      public:
+        StoreModuleWorkingEvent(StoreModule *storeModule);
+        auto process() -> void override;
+    };
+
+    StoreModuleWorkingEvent workingEvent;
+
+    vta::Instruction::MemoryInstruction *instruction;
+
   public:
     PARAMS(StoreModule);
 
     StoreModule(const Params &params);
+
+    auto notifyRead() -> void;
+
+    auto notifyWrite() -> void;
 };
 } // namespace gem5
 

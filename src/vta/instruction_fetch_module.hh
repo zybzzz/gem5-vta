78f3b30e8fa7af21f92492e15d2ff01d839b6975
zrq8 <lhz@live.cn>
2024-03-08 14:51:03 +0800

    vta: Merge InstructionFetchModule definition into header file
    Change-Id: I03595a7fe7fa1d108add7461fa36138d0b6f6b00

--- src/vta/instruction_fetch_module.hh
+++ src/vta/instruction_fetch_module.hh
@@ -1,12 +1,17 @@
 #ifndef INSTRUCTION_FETCH_MODULE_HH
 #define INSTRUCTION_FETCH_MODULE_HH
 
+#include "base/trace.hh"
+#include "debug/BaseVTAFlag.hh"
 #include "params/InstructionFetchModule.hh"
 #include "sim/sim_object.hh"
 #include "vta/instruction_queue.hh"
+#include "vta/vta.hh"
+#include "vta/vta_const.hh"
 
 namespace gem5
 {
+
 class InstructionFetchModule : public SimObject
 {
   private:
@@ -17,8 +22,52 @@ class InstructionFetchModule : public SimObject
   public:
     PARAMS(InstructionFetchModule);
 
-    InstructionFetchModule(const Params &params);
+    InstructionFetchModule(const Params &params) :
+        SimObject(params),
+        loadCommandQueue(params.load_command_queue),
+        computeCommandQueue(params.compute_command_queue),
+        storeCommandQueue(params.store_command_queue)
+    {
+        DPRINTF(BaseVTAFlag, "create InstructionFetchModule!\n");
+
+        vta::Instruction *instruction_vec{};
+        size_t instruction_count{};
+        // handle the situation with queue full
+        for (size_t pc{}; pc != instruction_count; ++pc) {
+            const auto instruction{instruction_vec[pc]};
+            const auto opcode{instruction.opcode()};
+            switch (opcode) {
+            case vta::Opcode::LOAD:
+                if (const auto memory_type{
+                        instruction.asMemoryInstruction().memory_type};
+                    memory_type == vta::MemoryId::INPUT ||
+                    memory_type == vta::MemoryId::WEIGHT) {
+                    DPRINTF(BaseVTAFlag, "instruction to load\n");
+                    // loadCommandQueue << instruction;
+                    loadCommandQueue->push(instruction);
+                } else {
+                    DPRINTF(BaseVTAFlag, "instruction to compute\n");
+                    // computeCommandQueue << instruction;
+                    computeCommandQueue->push(instruction);
+                }
+                break;
+            case vta::Opcode::STORE:
+                DPRINTF(BaseVTAFlag, "instruction to store\n");
+                // storeCommandQueue << instruction;
+                storeCommandQueue->push(instruction);
+                break;
+            case vta::Opcode::FINISH:
+                break;
+            default:
+                DPRINTF(BaseVTAFlag, "instruction to compute\n");
+                // computeCommandQueue << instruction;
+                computeCommandQueue->push(instruction);
+                break;
+            }
+        }
+    };
 };
+
 } // namespace gem5
 
 #endif

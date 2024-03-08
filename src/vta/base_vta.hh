6629a5a4c387268a4407f1900c52e4100e2354ed
zrq8 <lhz@live.cn>
2024-03-08 14:26:33 +0800

    vta: Merge BaseVTA definition into header file
    Change-Id: If9ddbe26a60b95dd2c85471efbfdeb9eb7ddddcf

--- src/vta/base_vta.hh
+++ src/vta/base_vta.hh
@@ -1,20 +1,36 @@
 #ifndef BASE_VTA_HH
 #define BASE_VTA_HH
 
+#include "base/trace.hh"
+#include "debug/BaseVTAFlag.hh"
 #include "params/BaseVTA.hh"
 #include "sim/sim_object.hh"
+#include "vta/vta.hh"
 
 namespace gem5
 {
 
 class BaseVTA : public SimObject
 {
+  private:
+    vta::CommandQueue loadCommandQueue{};
+    vta::CommandQueue computeCommandQueue{};
+    vta::CommandQueue storeCommandQueue{};
+
   public:
     PARAMS(BaseVTA);
 
-    BaseVTA(const Params &params);
+    BaseVTA(const Params &params) : SimObject(params)
+    {
+        DPRINTF(BaseVTAFlag, "===========================================\n");
+        DPRINTF(BaseVTAFlag, "initializing gem5-vta ... user configuration "
+                             "parameters are as follows: \n");
+        DPRINTF(BaseVTAFlag, "===========================================\n");
+    }
 
-    virtual void startup() override;
+    virtual void
+    startup() override
+    {}
 };
 
 } // namespace gem5

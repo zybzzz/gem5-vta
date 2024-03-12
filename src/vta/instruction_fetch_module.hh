b359b4ec0c1f09bf5144f6f588f8560a767440a0
zrq8 <lhz@live.cn>
2024-03-12 16:54:41 +0800

    vta: Add CommandQueue and Event for InstructionFetchModule
    Change-Id: Iab148e641e5d98cab644a125d6f723c4b2a674e6

--- src/vta/instruction_fetch_module.hh
+++ src/vta/instruction_fetch_module.hh
@@ -1,15 +1,23 @@
 #ifndef INSTRUCTION_FETCH_MODULE_HH
 #define INSTRUCTION_FETCH_MODULE_HH
 
+#include <cassert>
+#include <cstddef>
+#include <memory>
 #include <string>
 #include <string_view>
 
 #include "base/trace.hh"
+#include "base/types.hh"
 #include "debug/BaseVTAFlag.hh"
+#include "mem/packet.hh"
+#include "mem/packet_access.hh"
 #include "mem/port.hh"
+#include "mem/request.hh"
 #include "params/InstructionFetchModule.hh"
+#include "sim/cur_tick.hh"
 #include "sim/sim_object.hh"
-#include "vta/instruction_queue.hh"
+#include "vta/command_queue.hh"
 #include "vta/vta.hh"
 #include "vta/vta_const.hh"
 
@@ -19,6 +27,9 @@ namespace gem5
 class InstructionFetchModule : public SimObject
 {
   private:
+    size_t pc{};
+    vta::Instruction instruction;
+
     class InstructionPort : public RequestPort
     {
       private:
@@ -34,6 +45,14 @@ class InstructionFetchModule : public SimObject
         auto
         recvTimingResp(PacketPtr pkt) -> bool override
         {
+            delete pkt;
+            DPRINTF(BaseVTAFlag, "Instruction: %016lx%016lx\n",
+                *(uint64_t *)&owner.instruction.data[8],
+                *(uint64_t *)&owner.instruction.data[0]);
+            DPRINTF(
+                BaseVTAFlag, "Opcode: %d\n", (int)owner.instruction.opcode());
+            owner.schedule(owner.event, curTick());
+            DPRINTF(BaseVTAFlag, "Instruction fetch event scheduled\n");
             return true;
         }
 
@@ -46,9 +65,9 @@ class InstructionFetchModule : public SimObject
         {}
     } instruction_port;
 
-    InstructionQueue *loadCommandQueue;
-    InstructionQueue *computeCommandQueue;
-    InstructionQueue *storeCommandQueue;
+    CommandQueue &loadCommandQueue;
+    CommandQueue &computeCommandQueue;
+    CommandQueue &storeCommandQueue;
 
     class ProgressEvent : public Event
     {
@@ -61,7 +80,78 @@ class InstructionFetchModule : public SimObject
         auto
         process() -> void override
         {
-            owner.reschedule(this, curTick() + 1);
+            static enum class State {
+                FETCH,
+                DECODE,
+                LOAD,
+                COMPUTE,
+                STORE,
+            } state{};
+            switch (state) {
+            case State::FETCH: {
+                DPRINTF(BaseVTAFlag, "Fetch\n");
+                const auto req{std::make_shared<Request>(Addr{owner.pc},
+                    sizeof(vta::Instruction), Request::INST_FETCH, 3)};
+                auto packet{new Packet{req, MemCmd::ReadReq}};
+                DPRINTF(BaseVTAFlag, "Packet: %s\n", packet->print());
+                packet->dataStatic(&owner.instruction);
+                const auto ret{owner.instruction_port.sendTimingReq(packet)};
+                assert(ret);
+                state = State::DECODE;
+                return;
+            }
+            case State::DECODE: {
+                DPRINTF(BaseVTAFlag, "Decode\n");
+                owner.pc += sizeof(vta::Instruction);
+                const auto instruction{owner.instruction};
+                const auto opcode{instruction.opcode()};
+                switch (opcode) {
+                case vta::Opcode::LOAD:
+                    if (const auto memory_type{
+                            instruction.asMemoryInstruction().memory_type};
+                        memory_type == vta::MemoryId::INPUT ||
+                        memory_type == vta::MemoryId::WEIGHT) {
+                        state = State::LOAD;
+                    } else {
+                        state = State::COMPUTE;
+                    }
+                    break;
+                case vta::Opcode::STORE:
+                    state = State::STORE;
+                    break;
+                case vta::Opcode::FINISH:
+                    DPRINTF(BaseVTAFlag, "Finish\n");
+                    return;
+                default:
+                    state = State::COMPUTE;
+                    break;
+                }
+                process();
+                return;
+            }
+            case State::LOAD:
+                DPRINTF(BaseVTAFlag, "Load\n");
+                if (!owner.loadCommandQueue.full()) {
+                    owner.loadCommandQueue.push(owner.instruction);
+                    state = State::FETCH;
+                }
+                break;
+            case State::COMPUTE:
+                DPRINTF(BaseVTAFlag, "Compute\n");
+                if (!owner.computeCommandQueue.full()) {
+                    owner.computeCommandQueue.push(owner.instruction);
+                    state = State::FETCH;
+                }
+                break;
+            case State::STORE:
+                DPRINTF(BaseVTAFlag, "Store\n");
+                if (!owner.storeCommandQueue.full()) {
+                    owner.storeCommandQueue.push(owner.instruction);
+                    state = State::FETCH;
+                }
+                break;
+            }
+            owner.schedule(owner.event, curTick() + 1);
         }
     } event{*this};
 
@@ -71,48 +161,10 @@ class InstructionFetchModule : public SimObject
     InstructionFetchModule(const Params &params) :
         SimObject{params},
         instruction_port{params.name + ".instruction_port", *this},
-        loadCommandQueue{params.load_command_queue},
-        computeCommandQueue{params.compute_command_queue},
-        storeCommandQueue{params.store_command_queue}
-    {
-        DPRINTF(BaseVTAFlag, "create InstructionFetchModule!\n");
-
-        vta::Instruction *instruction_vec{};
-        size_t instruction_count{};
-        // handle the situation with queue full
-        for (size_t pc{}; pc != instruction_count; ++pc) {
-            const auto instruction{instruction_vec[pc]};
-            const auto opcode{instruction.opcode()};
-            switch (opcode) {
-            case vta::Opcode::LOAD:
-                if (const auto memory_type{
-                        instruction.asMemoryInstruction().memory_type};
-                    memory_type == vta::MemoryId::INPUT ||
-                    memory_type == vta::MemoryId::WEIGHT) {
-                    DPRINTF(BaseVTAFlag, "instruction to load\n");
-                    // loadCommandQueue << instruction;
-                    loadCommandQueue->push(instruction);
-                } else {
-                    DPRINTF(BaseVTAFlag, "instruction to compute\n");
-                    // computeCommandQueue << instruction;
-                    computeCommandQueue->push(instruction);
-                }
-                break;
-            case vta::Opcode::STORE:
-                DPRINTF(BaseVTAFlag, "instruction to store\n");
-                // storeCommandQueue << instruction;
-                storeCommandQueue->push(instruction);
-                break;
-            case vta::Opcode::FINISH:
-                break;
-            default:
-                DPRINTF(BaseVTAFlag, "instruction to compute\n");
-                // computeCommandQueue << instruction;
-                computeCommandQueue->push(instruction);
-                break;
-            }
-        }
-    }
+        loadCommandQueue{*params.load_command_queue},
+        computeCommandQueue{*params.compute_command_queue},
+        storeCommandQueue{*params.store_command_queue}
+    {}
 
     auto
     startup() -> void override

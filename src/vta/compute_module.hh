2db051d7ae206a65525b9ee1b3b710eade4a72bb
zrq8 <lhz@live.cn>
2024-03-18 20:14:15 +0800

    vta: Update ComputeModule
    Change-Id: I823cc9c236ab9eb7d6b7052aa397a0ad03e76ff9

--- src/vta/compute_module.hh
+++ src/vta/compute_module.hh
@@ -1,33 +1,47 @@
 #ifndef COMPUTE_MODULE_HH
 #define COMPUTE_MODULE_HH
 
+#include "base/trace.hh"
+#include "debug/BaseVTAFlag.hh"
 #include "params/ComputeModule.hh"
 #include "sim/sim_object.hh"
 #include "vta/buffer.hh"
-#include "vta/data_queue.hh"
-#include "vta/instruction_queue.hh"
+#include "vta/command_queue.hh"
+#include "vta/dependency_queue.hh"
 
 namespace gem5
 {
+
 class ComputeModule : public SimObject
 {
   private:
-    InstructionQueue *computeCommandQueue;
+    CommandQueue &computeCommandQueue;
 
-    DataQueue *computeToStoreQueue;
-    DataQueue *storeToComputeQueue;
-    DataQueue *computeToLoadQueue;
-    DataQueue *loadToComputeQueue;
+    DependencyQueue &loadToComputeQueue;
+    DependencyQueue &computeToLoadQueue;
+    DependencyQueue &computeToStoreQueue;
+    DependencyQueue &storeToComputeQueue;
 
-    Buffer *inputBuffer;
-    Buffer *weightBuffer;
-    Buffer *outputBuffer;
+    Buffer &inputBuffer;
+    Buffer &weightBuffer;
+    Buffer &outputBuffer;
 
   public:
     PARAMS(ComputeModule);
 
-    ComputeModule(const Params &params);
+    ComputeModule(const Params &params) :
+        SimObject{params},
+        computeCommandQueue{*params.compute_command_queue},
+        loadToComputeQueue{*params.load_to_compute_queue},
+        computeToLoadQueue{*params.compute_to_load_queue},
+        computeToStoreQueue{*params.compute_to_store_queue},
+        storeToComputeQueue{*params.store_to_compute_queue},
+        inputBuffer{*params.input_buffer},
+        weightBuffer{*params.weight_buffer},
+        outputBuffer{*params.output_buffer}
+    {}
 };
+
 } // namespace gem5
 
 #endif

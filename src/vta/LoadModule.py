626b9591c1a09c94e7889967f723c10d3b7b3395
zrq8 <lhz@live.cn>
2024-03-18 20:30:34 +0800

    vta: Update LoadModule
    Change-Id: Ia37899435d9332471e7ca78313e11f010be08c0f

--- src/vta/LoadModule.py
+++ src/vta/LoadModule.py
@@ -1,5 +1,8 @@
 from m5.params import *
 from m5.SimObject import SimObject
+from m5.objects.Buffer import *
+from m5.objects.CommandQueue import *
+from m5.objects.DependencyQueue import *
 
 
 class LoadModule(SimObject):
@@ -7,22 +10,14 @@ class LoadModule(SimObject):
     cxx_header = "vta/load_module.hh"
     cxx_class = "gem5::LoadModule"
 
-    load_command_queue = Param.InstructionQueue(NULL, "load command queue")
+    load_command_queue = Param.CommandQueue("load command queue")
 
-    compute_to_load_queue = Param.DataQueue(
-        NULL, "compute module to load module queue"
+    load_to_compute_queue = Param.DependencyQueue(
+        "load module to compute module queue"
     )
-    load_to_compute_queue = Param.DataQueue(
-        NULL, "load module to compute module queue"
+    compute_to_load_queue = Param.DependencyQueue(
+        "compute module to load module queue"
     )
 
-    input_buffer = Param.Buffer(NULL, "VTA input buffer")
-    weight_buffer = Param.Buffer(NULL, "VTA weight buffer")
-
-    polling_latency = Param.Cycles(10, "interval of polling")
-    wait_dataqueue_write_latency = Param.Cycles(
-        10, "time to wait dataqueue write"
-    )
-    wait_dataqueue_read_latency = Param.Cycles(
-        10, "time to wait dataqueue read"
-    )
+    input_buffer = Param.Buffer("VTA input buffer")
+    weight_buffer = Param.Buffer("VTA weight buffer")
--- src/vta/load_module.hh
+++ src/vta/load_module.hh
@@ -1,57 +1,117 @@
 #ifndef LOAD_MODULE_HH
 #define LOAD_MODULE_HH
 
-#include "base/types.hh"
+#include "base/trace.hh"
+#include "debug/BaseVTAFlag.hh"
 #include "params/LoadModule.hh"
 #include "sim/eventq.hh"
 #include "sim/sim_object.hh"
 #include "vta/buffer.hh"
-#include "vta/data_queue.hh"
-#include "vta/instruction_queue.hh"
-#include "vta/status.hh"
+#include "vta/command_queue.hh"
+#include "vta/dependency_queue.hh"
 
 namespace gem5
 {
+
 class LoadModule : public SimObject
 {
   private:
-    InstructionQueue *loadCommandQueue;
+    vta::MemoryInstruction instruction;
 
-    DataQueue *computeToLoadQueue;
-    DataQueue *loadToComputeQueue;
+    bool lastInstructionFinish{true};
 
-    Buffer *inputBuffer;
-    Buffer *weightBuffer;
+    CommandQueue &loadCommandQueue;
 
-    LoadModuleStatus status;
+    DependencyQueue &loadToComputeQueue;
+    DependencyQueue &computeToLoadQueue;
+
+    Buffer &inputBuffer;
+    Buffer &weightBuffer;
 
     class LoadModuleWorkingEvent : public Event
     {
       private:
-        LoadModule *loadModule;
+        LoadModule &owner;
 
       public:
-        LoadModuleWorkingEvent(LoadModule *loadModule);
-        auto process() -> void override;
-    };
+        LoadModuleWorkingEvent(LoadModule &owner) : owner(owner) {}
 
-    LoadModuleWorkingEvent workingEvent;
+        auto
+        process() -> void override
+        {
+            static enum class Status {
+                Normal,
+                WaitToRead,
+                WaitToWrite,
+            } status{Status::WaitToRead};
+            /* while (!owner.lastInstructionFinish ||
+                   !owner.loadCommandQueue.empty()) {
+                switch (status) {
+                case Status::WaitToRead:
+                    if (owner.lastInstructionFinish) {
+                        // get instruction from queue
+                        auto tickStruct = owner.loadCommandQueue.pop();
+                        owner.instruction =
+                            tickStruct.value.asMemoryInstruction();
+                        owner.fetchInstTime = tickStruct.time;
+                        owner.lastInstructionFinish = false;
+                    }
+                    if (owner.instruction.pop_next_dependence) {
+                        // check instruction flag then maybe read flag from
+                        // data queue if queue can't read, delay to next time
+                        if (owner.computeToLoadQueue.tryPop()) {
+                            status = Status::Normal;
+                        } else {
+                            owner.reschedule(this, curTick() + 1);
+                            return; // return to wait next
+                        }
+                    }
 
-    vta::Instruction::MemoryInstruction instruction;
-    Tick fetchInstTime;
+                    break;
+                case Status::Normal:
+                    // judge and do load
+                    break;
+                case Status::WaitToWrite:
+                    if (owner.instruction.push_next_dependence) {
+                        if (owner.loadToComputeQueue.tryPush()) {
+                            status = Status::WaitToRead;
+                            owner.lastInstructionFinish = true;
+                        } else {
+                            // check instruction flag then maybe write flag
+                            // from data queue if queue can't read, delay to
+                            // next time
+                            status = Status::WaitToWrite;
+                            owner.reschedule(this, curTick() + 1);
+                            return; // return to wait next
+                        }
+                    }
+                    break;
+                }
+            } */
 
-    Cycles pollingLatency;
-    Cycles waitDataQueueWriteLatency;
-    Cycles waitDataQueueReadLatency;
-
-    bool lastInstructionFinish;
+            owner.reschedule(this, curTick() + 1);
+        }
+    } workingEvent{*this};
 
   public:
     PARAMS(LoadModule);
 
-    LoadModule(const Params &params);
-    auto startup() -> void override;
+    LoadModule(const Params &params) :
+        SimObject{params},
+        loadCommandQueue{*params.load_command_queue},
+        loadToComputeQueue{*params.load_to_compute_queue},
+        computeToLoadQueue{*params.compute_to_load_queue},
+        inputBuffer{*params.input_buffer},
+        weightBuffer{*params.weight_buffer}
+    {}
+
+    virtual auto
+    startup() -> void override
+    {
+        schedule(workingEvent, curTick());
+    }
 };
+
 } // namespace gem5
 
 #endif

cafefa2039c06aca9680e635a8bb8437032f1514
zrq8 <lhz@live.cn>
2024-03-18 19:52:17 +0800

    vta: Update BaseVTA
    Change-Id: I611363286ce1d497e550731a9d49ddc283561963

--- src/vta/BaseVTA.py
+++ src/vta/BaseVTA.py
@@ -3,8 +3,8 @@ from m5.proxy import *
 from m5.SimObject import SimObject
 from m5.objects.Buffer import *
 from m5.objects.CommandQueue import *
-from m5.objects.DependencyQueue import *
 from m5.objects.ComputeModule import *
+from m5.objects.DependencyQueue import *
 from m5.objects.InstructionFetchModule import *
 from m5.objects.LoadModule import *
 from m5.objects.StoreModule import *
@@ -41,36 +41,33 @@ class BaseVTA(SimObject):
     instruction_fetch_module.load_command_queue = load_command_queue
     instruction_fetch_module.compute_command_queue = compute_command_queue
     instruction_fetch_module.store_command_queue = store_command_queue
-
     instruction_fetch_module = Param.InstructionFetchModule(
         instruction_fetch_module,
         "instruction fetch module",
     )
 
-    ## regular module
-    compute_module = Param.ComputeModule(ComputeModule(), "compute module")
-    load_module = Param.LoadModule(LoadModule(), "load module")
-    store_module = Param.StoreModule(StoreModule(), "store module")
+    load_module = LoadModule()
+    load_module.load_command_queue = load_command_queue
+    load_module.load_to_compute_queue = load_to_compute_queue
+    load_module.compute_to_load_queue = compute_to_load_queue
+    load_module.input_buffer = input_buffer
+    load_module.weight_buffer = weight_buffer
+    load_module = Param.LoadModule(load_module, "load module")
 
-    # compute module
+    compute_module = ComputeModule()
     compute_module.compute_command_queue = compute_command_queue
     compute_module.load_to_compute_queue = load_to_compute_queue
     compute_module.compute_to_load_queue = compute_to_load_queue
-    compute_module.store_to_compute_queue = store_to_compute_queue
     compute_module.compute_to_store_queue = compute_to_store_queue
+    compute_module.store_to_compute_queue = store_to_compute_queue
     compute_module.input_buffer = input_buffer
     compute_module.weight_buffer = weight_buffer
     compute_module.output_buffer = output_buffer
+    compute_module = Param.ComputeModule(compute_module, "compute module")
 
-    # load module
-    load_module.load_queue = load_queue
-    load_module.compute_to_load_queue = compute_to_load_queue
-    load_module.load_to_compute_queue = load_to_compute_queue
-    load_module.input_buffer = input_buffer
-    load_module.weight_buffer = weight_buffer
-
-    # store module
-    store_module.store_queue = store_queue
+    store_module = StoreModule()
+    store_module.store_command_queue = store_command_queue
     store_module.compute_to_store_queue = compute_to_store_queue
     store_module.store_to_compute_queue = store_to_compute_queue
     store_module.output_buffer = output_buffer
+    store_module = Param.StoreModule(store_module, "store module")
